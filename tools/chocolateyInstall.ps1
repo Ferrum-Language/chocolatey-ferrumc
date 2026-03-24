81e74ce54bbb436a1a8f8dc1f0e3c0c4dfbc2291
Ferrum-Language <noreply@ferrum-lang.org>
2026-03-23 21:19:21 -0300

    chore: update Windows SHA256 for v0.3.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,13 +2,13 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'ferrumc'
 $url64       = 'https://github.com/Ferrum-Language/Ferrum/releases/download/v0.3.0/ferrumc-v0.3.0-windows-x86_64.zip'
-$checksum64  = 'WILL_BE_UPDATED_WHEN_WINDOWS_CI_COMPLETES'
+$checksum64  = '4b332aee1b5d2eb954a25706d1eec1203b23cd4de4776493be833cb39e22518d'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage `
-  -PackageName   $packageName `
-  -Url64bit      $url64 `
-  -Checksum64    $checksum64 `
+  -PackageName    $packageName `
+  -Url64bit       $url64 `
+  -Checksum64     $checksum64 `
   -ChecksumType64 'sha256' `
-  -UnzipLocation $toolsDir
+  -UnzipLocation  $toolsDir
